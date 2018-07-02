149ecc15ff0a1175ff6ebf48428d5fe281264937
Kewde <kewde@particl.io>
2018-07-02 15:29:27 +0200

    Interface docs

--- src/interfaces/omp.ts
+++ src/interfaces/omp.ts
@@ -5,27 +5,57 @@ export interface MPA {
 
 }
 
+/**
+ * This is the most basic listing.
+ * It should only contain the necessary fields
+ * as documented in protocol.
+ */
 export interface MPA_LISTING extends MPA {
     label: string;
 }
 
+/**
+ * This is the extended listing.
+ * It can also include additional fields.
+ */
+export interface MPA_LISTING_EXT extends MPA_LISTING {
+    somefield: string;
+}
+
+/**
+ *  MPA_BID (buyer -> sender)
+ *  It includes their payment details and links to the listing.
+ */
 export interface MPA_BID extends MPA {
+    bid_nonce: string; // !implementation
     buyer_outputs: string[];
     buyer_pubkey: string[];
 }
 
+/**
+ *  MPA_ACCEPT (sender -> buyer)
+ *  It is the seller payment details.
+ */
 export interface MPA_ACCEPT extends MPA {
+    bid_nonce: string; // !implementation !protocol
     label: string;
 }
 
+/**
+ *  MPA_BID (buyer -> sender)
+ *  It includes their payment details and links to the listing.
+ */
 export interface MPA_LOCK extends MPA {
+    bid_nonce: string; // !implementation !protocol
+    signature: string;
+}
+
+export interface MPA_RELEASE_REQ  extends MPA { // !implementation !protocol
+    bid_nonce: string; // !implementation !protocol
     label: string;
 }
 
-export interface MPA_RELEASE  extends MPA{
-    label: string;
-}
-
-export interface MPA_LISTING extends MPA {
+export interface MPA_RELEASE_OK  extends MPA { // !implementation !protocol
+    bid_nonce: string;
     label: string;
 }
