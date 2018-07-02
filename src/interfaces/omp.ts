4f243a65dc9e755001106352f14f53ed7ca38838
Cube <cryptocube@particl.io>
2018-07-03 01:09:06 +0930

    Small fixes and removal of some fields

--- src/interfaces/omp.ts
+++ src/interfaces/omp.ts
@@ -18,63 +18,64 @@ export interface MPA_LISTING extends MPA {
  * It can also include additional fields.
  */
 export interface MPA_LISTING extends MPA {
-  "item": {
-    "hash": String, //item hash
-    "information": {
-      "title": String,
-      "short_description": String,
-      "long_description": String,
-      "category": String[],
-      "location": {
-        "country": String,
-        "address": String,
-        "gps": {
-          "lng": Number,
-          "lat": Number,
-          "marker_title": String,
-          "marker_text": String
+  item: {
+    hash: String, //item hash
+    information: {
+      title: String,
+      short_description: String,
+      long_description: String,
+      category: String[],
+      location: {
+        country: String,
+        address: String,
+        gps: {
+          lng: Number,
+          lat: Number,
+          marker_title: String,
+          marker_text: String
         }
       },
-      "shipping_destinations": String[],
-      "images": [
+      shipping_destinations: String[],
+      images: [
         {
-            "hash": String, // item hash
-            "data": [
+            hash: String, // item hash
+            data: [
                 {
-                   "protocol": String, // LOCAL |
-                   "encoding": String, // BASE64 | 
-                   "data": String,
-                   "id": Number
+                   protocol: String, // LOCAL |
+                   encoding: String, // BASE64 | 
+                   data: String,
+                   id: Number
                 }
             ]
         }
     },
-    "payment": {
-      "type": String, // SALE | FREE
-      "escrow": {
-        "type": String,
-        "ratio": {
-          "buyer": Number,
-          "seller": Number
+    payment: {
+      type: String, // SALE | FREE
+      escrow: {
+        type: String,
+        ratio: {
+          buyer: Number,
+          seller: Number
         }
       },
-      "cryptocurrency": [
+      cryptocurrency: [
         {
-          "currency": String, // PARTICL | BITCOIN
-          "base_price": Number,
-          "shipping_price": {
-            "domestic": Number,
-            "international": Number
+          currency: String, // PARTICL | BITCOIN
+          base_price: Number,
+          shipping_price: {
+            domestic: Number,
+            international: Number
           },
-          "address": {
-            "type": String, // NORMAL | 
-            "address": String
+          address: {
+            type: String, // NORMAL | 
+            address: String
           }
         }
       ]
     },
-    "messaging": [],
-    "objects": []
+    messaging: [],
+    //// rm !implementation
+    // objects: []
   }
 }
 
@@ -84,8 +85,8 @@ export interface MPA_LISTING extends MPA {
  */
 // matches MPA_ACCEPT
 export interface MPA_BID extends MPA {
-  "mpaction": {
-    action: "MPA_BID",
+  mpaction: {
+    action: MPA_BID,
     item: String, //item hash
     objects: [
       id: String
@@ -99,57 +100,57 @@ export interface MPA_BID extends MPA {
  *  It includes their payment details and links to the listing.
  */
 export interface MPA_BID extends MPA { // Extended
-  "mpaction": {
-    action: "MPA_BID",
+  mpaction: {
+    action: MPA_BID,
     item: string, // item hash
     objects: [
       {
-        "id": "buyerPubkey",
-        "value": String
+        id: buyerPubkey,
+        value: String
       },
       {
-        "id": "buyerChangeAddress",
-        "value": String
+        id: buyerChangeAddress,
+        value: String
       },
       {
-        "id": "buyerChange",
-        "value": Number
+        id: buyerChange,
+        value: Number
       },
       {
-        "id": "buyerAddress",
-        "value": String
+        id: buyerAddress,
+        value: String
       },
       {
-        "id": "ship.firstName",
-        "value": String
+        id: ship.firstName,
+        value: String
       },
       {
-        "id": "ship.lastName",
-        "value": String
+        id: ship.lastName,
+        value: String
       },
       {
-        "id": "ship.addressLine1",
-        "value": String
+        id: ship.addressLine1,
+        value: String
       },
       {
-        "id": "ship.addressLine2",
-        "value": String
+        id: ship.addressLine2,
+        value: String
       },
       {
-        "id": "ship.city",
-        "value": String
+        id: ship.city,
+        value: String
       },
       {
-        "id": "ship.state",
-        "value": String
+        id: ship.state,
+        value: String
       },
       {
-        "id": "ship.zipCode",
-        "value": String
+        id: ship.zipCode,
+        value: String
       },
       {
-        "id": "ship.country",
-        "value": String
+        id: ship.country,
+        value: String
       }
     ]
   }
@@ -161,8 +162,8 @@ export interface MPA_BID extends MPA { // Extended
  */
 // matches MPA_BID
 export interface MPA_ACCEPT extends MPA {
-  "mpaction": {
-    action: "MPA_ACCEPT",
+  mpaction: {
+    action: MPA_ACCEPT,
     item: String, //item hash
     objects: [
       id: String
@@ -176,45 +177,47 @@ export interface MPA_ACCEPT extends MPA {
  *  It is the seller payment details.
  */
 export interface MPA_ACCEPT extends MPA { // Extended
-  "mpaction": {
-    "action": "MPA_ACCEPT",
-    "item": "647dd1841eb669b9e923d521c6d5e66981bcf6ec07cd62a27574425691179eb8",
-    "objects": [
+  mpaction: {
+    action: MPA_ACCEPT,
+    item: 647dd1841eb669b9e923d521c6d5e66981bcf6ec07cd62a27574425691179eb8,
+    objects: [
       {
-        "id": "sellerOutputs",
-        "value": [
+        id: sellerOutputs,
+        value: [
           {
-            "txid": String,
-            "vout": Number,
-            "amount": Number
+            txid: String,
+            vout: Number,
+            amount: Number
           }
         ]
       },
+      //// rm !implementation
+      // {
+      //    id: buyerOutputs,
+      //    value: [
+      //     {
+      //       txid: String,
+      //       vout: Number,
+      //       amount: Number
+      //     }
+      //   ]
+      // },
       {
-        "id": "buyerOutputs",
-        "value": [
-          {
-            "txid": String,
-            "vout": Number,
-            "amount": Number
-          }
-        ]
+        id: sellerPubkey,
+        value: String
+      },
+      //// rm !implementation
+      // {
+      //   id: buyerPubkey,
+      //   value: String
+      // },
+      {
+        id: rawtx,
+        value: String
       },
       {
-        "id": "sellerPubkey",
-        "value": String
-      },
-      {
-        "id": "buyerPubkey",
-        "value": String
-      },
-      {
-        "id": "rawtx",
-        "value": String
-      },
-      {
-        "id": "orderHash",
-        "value": String
+        id: orderHash,
+        value: String
       }
     ]
   }
@@ -222,25 +225,28 @@ export interface MPA_ACCEPT extends MPA { // Extended
 
 // matches MPA_RELEASE, mostly
 export interface MPA_LOCK extends MPA {
-  "mpaction": {
-    "action": "MPA_LOCK",
-    "item": String, // item hash
-    "info": {},
-    "escrow": {
-      "type": "lock",
-      "rawtx": String
+  mpaction: {
+    action: MPA_LOCK,
+    item: String, // item hash
+    info: {},
+    escrow: {
+      type: lock,
+      rawtx: String
     }
   }
 }
 
 // matches MPA_LOCK, mostly
 export interface MPA_RELEASE  extends MPA{
-  "mpaction": {
-    "action": "MPA_RELEASE",
-    "item": String, // item hash
-    "escrow": {
-      "type": "release",
-      "rawtx": String
+  mpaction: {
+    action: MPA_RELEASE,
+    item: String, // item hash
+    escrow: {
+      type: release,
+      //// rm !implementation
+      // rawtx: String
+      //// add !implementation
+      signature: String
     }
   }
 }
