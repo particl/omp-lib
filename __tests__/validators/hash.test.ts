162769fa37c408f22edaddc24273e7522e840577
Kewde <kewde@particl.io>
2018-07-08 19:35:05 +0200

    Deterministic hasher

---
+++ __tests__/validators/hash.test.ts
@@ -0,0 +1,225 @@
+import { hash, hashListing, deepSortObject } from "../../src/hasher/hash";
+
+const deepNestOne = {
+    "b": {
+        "outerarray": [
+            {
+                "innerarray": [
+                    "b",
+                    "d",
+                    "a",
+                    {
+                        "to test aye": "two",
+                        "alongerstring": "one"
+                    },
+                    {
+                        "neveragain": "two",
+                        "seriously": "one"
+                    },
+                    {
+                        "ihateparsers": "two",
+                        "aalongerstring": "one"
+                    }
+                ]
+            },
+            "alevel2",
+            "dlevel2",
+            "blevel2",
+            "clevel2"
+        ],
+        "a": "level1"
+    },
+    "a": 0,
+    "d": 5
+};
+
+const deepNestTwo = {
+    "b": {
+        "outerarray": [
+            {
+                "innerarray": [
+                    "b",
+                    "a",
+                    "d",
+                    {
+                        "to test aye": "two",
+                        "alongerstring": "one"
+                    },
+                    {
+                        "ihateparsers": "two",
+                        "aalongerstring": "one"
+                    },
+                    {
+                        "neveragain": "two",
+                        "seriously": "one"
+                    }
+                ]
+            },
+            "blevel2",
+            "clevel2",
+            "alevel2",
+            "dlevel2"
+        ],
+        "a": "level1"
+    },
+    "d": 5,
+    "a": 0
+};
+
+test('normalize and hash', () => {
+    let output = "one"
+    let two = "two"
+    try {
+        console.log(JSON.stringify(deepSortObject(deepNestOne), null, 4));
+        console.log(JSON.stringify(deepSortObject(deepNestTwo), null, 4));
+
+        output = hash(deepNestOne)
+        two = hash(deepNestTwo)
+        // console.log(output + " === " + two);
+    } catch (e) {
+        console.log(e);
+    }
+    expect(output).toBe(two);
+});
+
+
+
+const ok = JSON.parse(
+    `{
+        "version": "0.1.0.0",
+        "action": {
+            "type": "MPA_LISTING_ADD",
+            "item": {
+              "information": {
+                "title": "a 6 month old dog",
+                "shortDescription": "very cute",
+                "longDescription": "not for eating",
+                "category": [
+                    "Animals"
+                ]
+              },
+              "payment": {
+                "type": "SALE",
+                "escrow": {
+                  "type": "MAD",
+                  "ratio": {
+                    "buyer": 100,
+                    "seller": 100
+                  }
+                },
+                "cryptocurrency": [
+                  {
+                    "currency": "PART",
+                    "basePrice": 10
+                  }
+                ]
+              },
+              "messaging": [
+                {
+                  "protocol": "TODO",
+                  "publicKey": "TODO"
+                }
+              ]
+            }
+        }
+    }`);
+
+const data_image = "fdgnihdqfgojsodhgofjsgishdfgihsdfpoghsidghipfghidshgyiyriehrtsugquregfudfugbfugd";
+
+const ok_full_img_data = JSON.parse(JSON.stringify(ok));
+ok_full_img_data.action.item.information.images = [];
+ok_full_img_data.action.item.information.images.push({
+    hash: hash(data_image),
+    data: [
+        {
+            protocol: 'LOCAL',
+            id: 'somename.png',
+            encoding: 'BASE64',
+            data: data_image
+        }
+    ]
+});
+
+const ok_less_img_data = JSON.parse(JSON.stringify(ok));
+ok_less_img_data.action.item.information.images = [];
+ok_less_img_data.action.item.information.images.push({
+    hash: hash(data_image),
+    data: [
+        {
+            protocol: 'LOCAL',
+            id: 'somename.png',
+            encoding: 'BASE64'
+        }
+    ]
+});
+/*
+test('compare hashes of two listings full vs less local images', () => {
+    let output = "one"
+    let two = "two"
+    try {
+        output = hash(ok_full_img_data)
+        two = hash(ok_less_img_data)
+        console.log(output + " === " + two);
+    } catch (e) {
+        console.log(e);
+    }
+    expect(output).toBe(two);
+});*/
+
+
+
+
+// different order of the keys
+const ok_two = JSON.parse(
+    `{
+        "version": "0.1.0.0",
+        "action": {
+            "type": "MPA_LISTING_ADD",
+            "item": {
+              "information": {
+                "longDescription": "not for eating",
+                "shortDescription": "very cute",
+                "title": "a 6 month old dog",
+                "category": [
+                    "Animals"
+                ]
+              },
+              "payment": {
+                "type": "SALE",
+                "escrow": {
+                  "ratio": {
+                    "buyer": 100,
+                    "seller": 100
+                  },
+                  "type": "MAD"
+                },
+                "cryptocurrency": [
+                  {
+                    "currency": "PART",
+                    "basePrice": 10
+                  }
+                ]
+              },
+              "messaging": [
+                {
+                  "publicKey": "TODO",
+                  "protocol": "TODO"
+                }
+              ]
+            }
+        }
+    }`);
+
+
+test('compare hashes of two listings full vs less local images', () => {
+    let output = "one"
+    let two = "two"
+    try {
+        output = hashListing(ok)
+        two = hashListing(ok_two)
+        console.log(output + " === " + two);
+    } catch (e) {
+        console.log(e);
+    }
+    expect(output).toBe(two);
+});
---
+++ src/hasher/hash.ts
@@ -0,0 +1,162 @@
+import { sha256 } from 'js-sha256';
+import { isString, isObject, isArray, isNumber } from '../format-validators/util';
+import { MPA_EXT_LISTING_ADD } from '../interfaces/omp'
+import { ContentReference, ProtocolDSN } from '../interfaces/dsn'
+
+export function hash(v: any): string {
+    if(typeof v === 'undefined') {
+        throw new Error('hash(): value is undefined');
+    }
+
+    if(isObject(v)){
+        return hashObject(v);
+    } else {
+        return sha256(v);
+    }
+
+}
+
+function hashObject(unordered: object): string {
+    const sorted = deepSortObject(unordered);
+    let keyHashes = sha256.update('OpenMarketProtocol');
+    // const ordered = deepSortObject(unordered);
+    // console.log(JSON.stringify(ordered, null, 4));
+    deep(sorted, (toHash) => {
+        keyHashes.update(toHash);
+    });
+
+    return sha256(keyHashes.array().join());
+}
+
+/*export function deep(unordered: any, callback: Function): object {
+    // order the keys alphabetically!
+    let result = {};
+    let ordered = undefined;
+    if(isArray(unordered)) {
+        ordered = unordered.sort();
+    } else if(isObject(unordered)) {
+        ordered = Object.keys(unordered).sort();
+    } else {
+        return callback("",unordered);
+    }
+
+    ordered.forEach(function(key) {
+        // if value is object, recursively sort it
+        if(isArray(unordered[key])) {
+            result[key] = [];
+            unordered[key].sort().forEach((elem) => {
+                result[key].push(callback(key, deep(elem, callback)));
+            })
+        }
+        else if(isObject(unordered[key])) {
+            result[key] = callback(key, deep(unordered[key], callback))
+        } else {
+            result[key] = callback(key, unordered[key]);
+        }
+    });
+
+    return result;
+} */
+
+export function hashListing(l: MPA_EXT_LISTING_ADD) {
+
+    // remove the local image data from the hashing
+    // the ContentReference hash already provides us
+    // with authentication for the data
+    if(l.action.item.information.images) {
+        l.action.item.information.images.forEach((img: ContentReference) => {
+            img.data.forEach((dsn) => {
+                if(dsn.protocol !== ProtocolDSN.LOCAL) {
+                    delete dsn.data;
+                }
+            });
+        })
+    } 
+
+    // console.log(JSON.stringify(l, null, 4));
+
+    return hash(l);
+
+}
+
+
+export function deepSortObject(unordered: any): any {
+    // order the keys alphabetically!
+    let result = {};
+    let ordered = undefined;
+    if(isArray(unordered)) {
+        ordered = unordered.sort();
+    } else if(isObject(unordered)) {
+        ordered = Object.keys(unordered).sort();
+    } else {
+        return unordered;
+    }
+
+    ordered.forEach(function(key) {
+        // if value is object, recursively sort it
+        if(isArray(unordered[key])) {
+            result[key] = [];
+            unordered[key].forEach((elem) => {
+                result[key].push(deepSortObject(elem));
+            })
+
+            result[key] = result[key].sort(deepCompare);
+        }
+        else if(isObject(unordered[key])) {
+            result[key] = deepSortObject(unordered[key])
+        } else {
+            result[key] = unordered[key];
+        }
+    });
+
+    return result;
+}
+
+
+function deep(sorted: any, callback: Function, parentKey?: string) {
+
+    parentKey = parentKey ? (parentKey + ":") : "";
+    if(isArray(sorted)) {
+        sorted.forEach(elem => {
+            deep(elem, callback, parentKey);
+        });
+    } else if(isObject(sorted)) {
+        Object.keys(sorted).forEach((key) => {
+            const childKey =  parentKey + key;
+            deep(sorted[key], callback, childKey);
+        });
+    } else {
+        const toHash = parentKey + sorted;
+        console.log(toHash);
+        callback(toHash)
+    }
+}
+
+/**
+ * Compares a string, number, and objects.
+ * If it's an object, grab the first key.
+ * Make sure to use _sorted_ objects for deterministic
+ * behavior.
+ * @param a object, string, number
+ * @param b object, string, number
+ */
+function deepCompare (a, b) {
+
+    if(isObject(a)) {
+        a = Object.keys(a)[0];
+    }
+
+    if(isObject(b)) {
+        b = Object.keys(b)[0];
+    }
+
+    if(a > b) {
+        return -1
+    }
+
+    if (b > a) {
+        return 1
+    }
+
+    return 0;
+}
