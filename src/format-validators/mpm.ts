14d95ec4393f76ee241ae300f172f4d00a7c3db9
Kewde <kewde@particl.io>
2018-07-12 16:15:22 +0200

    add mpm

---
+++ src/format-validators/mpm.ts
@@ -0,0 +1,30 @@
+import { MPA, MPM } from "../interfaces/omp"
+import { MPAction } from "../interfaces/omp-enums";
+
+
+export class FV_MPM {
+
+    constructor() {
+    }
+
+    static validate(msg: MPM): boolean {
+        if (!msg.version) {
+            throw new Error('version: missing');
+        }
+
+        if (!msg.action) {
+            throw new Error('action: missing');
+        }
+
+        if (!msg.action.type) {
+            throw new Error('action.type: missing');
+        }
+
+        if (!(msg.action.type in MPAction)) {
+            throw new Error('action.type: unrecognized value');
+        }
+
+        return true;
+    }
+
+}
