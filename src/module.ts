4b5948c862a559ec332a4d325fd19be756e907e6
Cube <cryptocube@particl.io>
2018-08-22 19:44:04 +1000

    Exports

---
+++ src/module.ts
@@ -0,0 +1,22 @@
+// /src/interfaces
+export { KVS } from './interfaces/common';
+export { BidConfiguration } from './interfaces/configs';
+export { Output, ToBeOutput, ISignature, CryptoType, CryptoAddressType, CryptoAddress, CryptoAmount } from './interfaces/crypto';
+export { DSN, ContentReference, ProtocolDSN } from './interfaces/dsn';
+export { MPM, MPA, MPA_LISTING_ADD, MPA_EXT_LISTING_ADD, MPA_BID, MPA_ACCEPT, MPA_LOCK, MPA_RELEASE, MPA_REFUND } from './interfaces/omp';
+export { MPAction, PaymentType, EscrowType } from './interfaces/omp-enums';
+
+// /omp-lib/_tests_/buyflow-multisig.test.ts
+import { OpenMarketProtocol } from "./omp";
+import { CryptoType } from "./interfaces/crypto";
+import { toSatoshis, fromSatoshis } from "./util";
+import { FV_MPA_BID } from "./format-validators/mpa_bid";
+import { FV_MPA_ACCEPT } from "./format-validators/mpa_accept";
+import { FV_MPA_LOCK } from "./format-validators/mpa_lock";
+import { FV_MPA_RELEASE } from "./format-validators/mpa_release";
+import { FV_MPA_REFUND } from "./format-validators/mpa_refund";
+
+// /test/rpc.stub.ts
+import { Rpc } from "../src/abstract/rpc";
+import { isNonNegativeNaturalNumber, isValidPrice } from "../src/util";
+import { TransactionBuilder } from "./transaction-builder/transaction";
