6eb3a4894df693bb8b5b065f405c803f02c5efdd
Juha Kovanen <juha.kovanen@gmail.com>
2018-10-10 06:54:57 +0300

    fixed lint errors on IMultiSigBuilder

--- src/abstract/transactions.ts
+++ src/abstract/transactions.ts
@@ -1,11 +1,11 @@
 import { MPM } from '../interfaces/omp';
 
 export interface IMultiSigBuilder {
-    bid(listing: MPM, bid: MPM): Promise<MPM>
-    accept(listing: MPM, bid: MPM, accept: MPM): Promise<MPM>
-    release(listing: MPM, bid: MPM, accept: MPM, release: MPM): Promise<MPM>
-    lock(listing: MPM, bid: MPM, accept: MPM, lock: MPM): Promise<MPM>
-    refund(listing: MPM, bid: MPM, accept: MPM, lock: MPM, refund: MPM): Promise<MPM>
+    bid(listing: MPM, bid: MPM): Promise<MPM>;
+    accept(listing: MPM, bid: MPM, accept: MPM): Promise<MPM>;
+    release(listing: MPM, bid: MPM, accept: MPM, release: MPM): Promise<MPM>;
+    lock(listing: MPM, bid: MPM, accept: MPM, lock: MPM): Promise<MPM>;
+    refund(listing: MPM, bid: MPM, accept: MPM, lock: MPM, refund: MPM): Promise<MPM>;
 }
 
 export interface ITransactionBuilder {
