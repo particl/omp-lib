6d14466076286a7012e8d53366334fc63d950aa5
Juha Kovanen <juha.kovanen@gmail.com>
2018-10-10 06:29:08 +0300

    fixed lint errors on FV_MPA_RELEASE

--- src/format-validators/mpa_release.ts
+++ src/format-validators/mpa_release.ts
@@ -1,21 +1,18 @@
-import { MPA_RELEASE, MPM } from "../interfaces/omp"
-import { MPAction, EscrowType } from "../interfaces/omp-enums";
-import { isObject, isArray, isString, isSHA256Hash } from "../util";
+import { MPA_RELEASE, MPM } from '../interfaces/omp';
+import { MPAction, EscrowType } from '../interfaces/omp-enums';
+import { isObject, isArray, isString, isSHA256Hash } from '../util';
 
-import { FV_CRYPTO } from "./crypto";
-import { FV_MPM} from "./mpm";
-import { FV_MPA_RELEASE_ESCROW_MULTISIG } from "./escrow/multisig";
+import { FV_CRYPTO } from './crypto';
+import { FV_MPM } from './mpm';
+import { FV_MPA_RELEASE_ESCROW_MULTISIG } from './escrow/multisig';
 
 export class FV_MPA_RELEASE {
 
-    constructor() {
-    }
-
-    static validate(msg: MPM): boolean {
+    public static validate(msg: MPM): boolean {
         // validate base class
         FV_MPM.validate(msg);
 
-        const action = <MPA_RELEASE>msg.action;
+        const action = <MPA_RELEASE> msg.action;
         const seller = action.seller;
 
         if (!isString(action.type)) {
@@ -50,6 +47,10 @@ export class FV_MPA_RELEASE {
         }
 
         return true;
+    }
+
+    constructor() {
+        //
     }
 
 }
