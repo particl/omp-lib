a9766a32e6f7761b796f3225d9c6e58d4204a02f
Juha Kovanen <juha.kovanen@gmail.com>
2018-10-10 05:45:43 +0300

    fixed lint errors on OpenMarketProtocol

--- src/omp.ts
+++ src/omp.ts
@@ -1,27 +1,18 @@
-import * as WebRequest from 'web-request';
-
-import "reflect-metadata";
+import 'reflect-metadata';
 import { injectable, Container, interfaces } from 'inversify';
 import { TYPES } from './types';
-
-import { Rpc, ILibrary } from './abstract/rpc';
+import { Rpc, ILibrary, Rpc } from './abstract/rpc';
 import { MPM } from './interfaces/omp';
-
 import { BidConfiguration } from './interfaces/configs';
-import { IBid } from './abstract/actions'
+import { IBid } from './abstract/actions';
 import { Bid } from './bid';
-
 import { CryptoType } from './interfaces/crypto';
-
 import { IMultiSigBuilder } from './abstract/transactions';
 import { MultiSigBuilder } from './transaction-builder/multisig';
 
 import { strip } from './util';
 
-//import { bid as createBid } from './bid';
-
-
-//@injectable()
+// @injectable()
 export class OpenMarketProtocol {
 
     // public TxLibs: Object = {};
@@ -29,72 +20,70 @@ export class OpenMarketProtocol {
 
     constructor() {
         this.container = new Container();
-
         this.setup();
     }
 
-    /**
-     *  Setup the container.
-     */
-    private setup() {
-        // This is our library factory
-        // it returns the Rpc libraries that we injected below (cfr. inject() ).
-        // based on a cryptocurrency: CryptoType
-        this.container.bind<ILibrary>(TYPES.Library).toFactory<Rpc>(
-            (ctx: interfaces.Context) => {
-                return (cryptocurrency: CryptoType) => {
-                    const lib = ctx.container.getNamed<Rpc>(TYPES.Rpc, cryptocurrency)
-                    return lib;
-                };
-            });
-        
-        this.container.bind<IBid>(TYPES.Bid).to(Bid);
-        this.container.bind<IMultiSigBuilder>(TYPES.MultiSigBuilder).to(MultiSigBuilder);
-    }
-
-    
     /**
      * Bind an Rpc service for a given cryptocurrency.
      * @param cryptocurrency The currency for which this service works.
      * @param service Rpc service
      */
-    public inject(cryptocurrency: CryptoType, service: any) {
+    public inject(cryptocurrency: CryptoType, service: any): void {
         // Bind an _instance_ (constant value)
         // to the container.
         // and give it the name of the cryptocurrency.
         this.container.bind<Rpc>(TYPES.Rpc).toConstantValue(service).whenTargetNamed(cryptocurrency.toString());
     }
 
-    public async bid(config: BidConfiguration, listing: MPM) {
+    public async bid(config: BidConfiguration, listing: MPM): Promise<MPM> {
         const bid = this.container.get<IBid>(TYPES.Bid);
-        return bid.bid(config, listing);
+        return await bid.bid(config, listing);
     }
 
-    public async accept(listing: MPM, bid: MPM) {
+    public async accept(listing: MPM, bid: MPM): Promise<MPM> {
         const action = this.container.get<IBid>(TYPES.Bid);
-        return action.accept(listing, bid);
+        return await action.accept(listing, bid);
     }
 
-    public async lock(listing: MPM, bid: MPM, accept: MPM) {
+    public async lock(listing: MPM, bid: MPM, accept: MPM): Promise<MPM> {
         const action = this.container.get<IBid>(TYPES.Bid);
-        return action.lock(listing, bid, accept);
+        return await action.lock(listing, bid, accept);
     }
 
-    public async release(listing: MPM, bid: MPM, accept: MPM, release?: MPM) {
+    public async release(listing: MPM, bid: MPM, accept: MPM, release?: MPM): Promise<MPM> {
         const action = this.container.get<IBid>(TYPES.Bid);
-        return action.release(listing, bid, accept, release);
+        return await action.release(listing, bid, accept, release);
     }
 
-    public async refund(listing: MPM, bid: MPM, accept: MPM, lock: MPM, refund?: MPM) {
+    public async refund(listing: MPM, bid: MPM, accept: MPM, lock: MPM, refund?: MPM): Promise<MPM> {
         const action = this.container.get<IBid>(TYPES.Bid);
-        return action.refund(listing, bid, accept, lock, refund);
+        return await action.refund(listing, bid, accept, lock, refund);
     }
 
-    public strip(msg: MPM) {
+    public strip(msg: MPM): MPM {
         return strip(msg);
     }
 
-    public rpc(cryptocurrency: CryptoType) {
+    public rpc(cryptocurrency: CryptoType): Rpc {
         return this.container.getNamed<Rpc>(TYPES.Rpc, cryptocurrency);
+    }
+
+    /**
+     *  Setup the container.
+     */
+    private setup(): void {
+        // This is our library factory
+        // it returns the Rpc libraries that we injected below (cfr. inject() ).
+        // based on a cryptocurrency: CryptoType
+        this.container.bind<ILibrary>(TYPES.Library).toFactory<Rpc>(
+            (ctx: interfaces.Context) => {
+                return (cryptocurrency: CryptoType) => {
+                    const lib = ctx.container.getNamed<Rpc>(TYPES.Rpc, cryptocurrency);
+                    return lib;
+                };
+            });
+
+        this.container.bind<IBid>(TYPES.Bid).to(Bid);
+        this.container.bind<IMultiSigBuilder>(TYPES.MultiSigBuilder).to(MultiSigBuilder);
     }
 }
