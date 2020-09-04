5a708515e93a3b0beed06890f555961353111ea3
Juha Kovanen <juha.kovanen@gmail.com>
2020-09-04 14:23:08 +0300

    hashable validation fix

--- src/format-validators/hashable.ts
+++ src/format-validators/hashable.ts
@@ -1,11 +1,12 @@
 // tslint:disable:max-line-length
 import { Validator } from 'semverv/dist/validator';
 import { HashableObject } from '../hasher/hashable';
+import { HashableConfig } from '../interfaces/configs';
 
 export class HashableValidator extends Validator {
-    public validate(hashable: HashableObject): boolean {
+    public validate(hashable: HashableObject, config: HashableConfig): boolean {
         // validate that the result contains all the fields defined in config
-        for (const configField of this.config.fields) {
+        for (const configField of config.fields) {
             if (!hashable.hasOwnProperty(configField.to)) {
                 throw new Error(configField.to + ': missing');
             }
--- src/hasher/hash.ts
+++ src/hasher/hash.ts
@@ -18,7 +18,7 @@ export class ConfigurableHasher {
      */
     public static hash(objectToHash: any, config: HashableConfig): string {
         const hashable: HashableObject = ConfigurableHasher.toHashable(objectToHash, config);
-        new HashableValidator(config).valid(hashable);
+        new HashableValidator(config).valid(hashable, config);
         // console.log('OMP_LIB: ConfigurableHasher.hash(), in: ', JSON.stringify(hashable, null, 2));
         const result = this.hashInner(hashable);
         // console.log('OMP_LIB: ConfigurableHasher.hash(), out: ', result);
